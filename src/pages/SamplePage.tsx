a2df5ec93d4ddd32edb12dff7e9d350b80e4734e
Larry Barrios Jr. <github@larrybarriosjr.com>
2024-12-02 23:03:34 +0800

    Add sample query

--- src/pages/SamplePage.tsx
+++ src/pages/SamplePage.tsx
@@ -1,7 +1,11 @@
+import useSampleQuery from "~/hooks/useSampleQuery"
 import useSampleStore from "~/store/sample"
 
 const SamplePage = () => {
+	const { data, isLoading } = useSampleQuery()
 	const [addSample] = useSampleStore((state) => [state.addSample])
+
+	console.log(isLoading ? "loading..." : data)
 
 	return <button onClick={() => addSample("a")}>Hi</button>
 }
